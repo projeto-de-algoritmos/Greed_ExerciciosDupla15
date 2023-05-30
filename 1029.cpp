0aa602ff91a448d5aaa46a74575cbfef9f3c94bc
Rodrigo Braz F. Gontijo <97805440+rodrigogontijoo@users.noreply.github.com>
2023-05-30 02:36:29 -0300

    Create 1029.cpp

---
+++ 1029.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int twoCitySchedCost(vector<vector<int>>& costs) {
+
+        vector<pair<int,int>> v;
+        int n = costs.size();
+
+        // no first, a diferença aCosti - bCosti e no second a posição que está no vector costs
+        for(int i = 0; i < n; i++)
+            v.push_back(make_pair(costs[i][0] - costs[i][1], i));
+        
+        // orderna por aCosti - bCosti
+        sort(v.begin(),v.end());
+
+        int sum = 0;
+
+        // soma os custos para a cidade A
+        for(int i = 0; i < n/2; i++)
+            sum+=costs[v[i].second][0];
+        
+        // soma os custos para a cidade B
+        for(int i = n/2; i < n; i++)
+            sum+=costs[v[i].second][1];
+        
+
+        return sum;
+    }
+};
